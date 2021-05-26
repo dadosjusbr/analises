049326a04c4580d8044a4a3620fcc03fbf1f3aa3
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2021-05-26 13:15:02 -0300

    Passa a recuperar tudo de todos os órgãos disponíveis no dadosjus

--- src/fetch.sh
+++ src/fetch.sh
@@ -9,7 +9,8 @@ BASE_URL="https://cloud5.lsd.ufcg.edu.br:8080/swift/v1/dadosjusbr"
 anos=`seq 2018 2020`
 
 # LISTA DE ÓRGÃOS
-orgaos="trepb mppb trt13 tjpb mpf mpm mprj mprs"
+#orgaos="trepb mppb trt13 tjpb mpf mpm mprj mprs"
+orgaos=`awk -F, 'NR > 1 {print $2}' ../ready/orgaos-cobertos.csv`
 
 for ano in $anos; do
     for mes in `seq 1 12`; do
---
+++ src/lista_existentes.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(jsonlite)
+
+federais = fromJSON("https://dadosjusbr.org/uiapi/v1/orgao/Federal")$Agency$Name
+
+pb = fromJSON("https://dadosjusbr.org/uiapi/v1/orgao/PB")$Agency$Name
+
+orgaos_lugar <- function(lugar){
+  message("Procurando órgãos: ", lugar)
+  url_lugar = stringr::str_glue("https://dadosjusbr.org/uiapi/v1/orgao/{lugar}")
+  jsonlite::fromJSON(url_lugar)$Agency$Name
+}
+
+cobertos = c("Federal", "AL", "BA", "CE", "ES", "GO", "MA", "MG", "PB", "PR", "PE", "PI", "RN", "RS", "RJ", "SP", "SE", "TO")
+
+existem = tibble(local = cobertos) %>% 
+  mutate(orgao = map(local, orgaos_lugar))
+
+existem %>% 
+  unnest(orgao) %>% 
+  readr::write_csv(here::here("dados/ready/orgaos-cobertos.csv"))
